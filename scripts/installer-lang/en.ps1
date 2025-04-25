0a8857762993542c2286c415ffe563914fc0f29b
fix.dev <fixdevelopment1@gmail.com>
2025-04-25 23:21:02 +0330

    Update en.ps1

--- scripts/installer-lang/en.ps1
+++ scripts/installer-lang/en.ps1
@@ -1,8 +1,8 @@
 [PSCustomObject]@{
     Welcome         = "
-╔══════════════════════════════╗
+╔═══════════════════════════════╗
 ║ Welcome to Spofix for Windows ║
-╚══════════════════════════════╝"
+╚═══════════════════════════════╝"
     Incorrect       = "Oops, an incorrect value,"
     Incorrect2      = "enter again through "
     Download        = "Error downloading"
