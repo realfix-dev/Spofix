7f7e4a24bb17606d21c3fe375f4ff057c8c1c484
fix.dev <fixdevelopment1@gmail.com>
2025-04-26 15:15:51 +0330

    Add files via upload

---
+++ sample-themes/DarkBlue/theme.js
@@ -0,0 +1,9 @@
+(() => {
+  const script = document.createElement("SCRIPT");
+  script.setAttribute("type", "text/javascript");
+  script.setAttribute(
+    "src",
+    "https://example.com/DarkBlue/darkblue.js"
+  );
+  document.head.appendChild(script);
+})(); 
